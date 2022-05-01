be64e6bb35fcca84e31594993bde8bcd0e37be06
Таисия Титова <melodi63@yandex.ru>
2022-05-01 21:15:08 +0200

    Extract pipeline creation to a separate module

---
+++ src/9_evaluation_selection_project/pipeline.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def create_pipeline(random_state: int) -> Pipeline:
+    scaler = StandardScaler()
+    classifier = LogisticRegression(random_state=random_state)
+    pipeline = Pipeline(steps=[("scaler", scaler), ("classifier", classifier)])
+    return pipeline
