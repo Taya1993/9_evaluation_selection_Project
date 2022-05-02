d8c8b1a9f9237b0c240dee18a7f259c762db2241
Таисия Титова <melodi63@yandex.ru>
2022-05-03 00:09:32 +0200

    Task №8 experiments with models (2) and selecting features(2) and other params

--- src/9_evaluation_selection_project/pipeline.py
+++ src/9_evaluation_selection_project/pipeline.py
@@ -1,22 +1,46 @@
 from pathlib import Path
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel, VarianceThreshold
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
-def create_pipeline(
-    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
+def create_pipeline_LogisticRegression(
+    use_scaler: bool, max_iter: int, logreg_C: float, use_feature_selection: int, random_state: int
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
+    if use_feature_selection == 1:
+        pipeline_steps.append(("feature_selection", SelectFromModel(RandomForestClassifier(random_state=2022))))
+    if use_feature_selection == 2:
+        pipeline_steps.append(("feature_selection", VarianceThreshold(threshold = 0.20)))
     pipeline_steps.append(
         (
             "classifier",
             LogisticRegression(
                 random_state=random_state, max_iter=max_iter, C=logreg_C
             ),
+        )
+    )
+    return Pipeline(steps=pipeline_steps)
+
+def create_pipeline_RandomForest(
+    use_scaler: bool, n_estimators: int, use_feature_selection: int, random_state: int
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    if use_feature_selection == 1:
+        pipeline_steps.append(("feature_selection", SelectFromModel(RandomForestClassifier(random_state=2022))))
+    if use_feature_selection == 2:
+        pipeline_steps.append(("feature_selection", VarianceThreshold(threshold = 0.20)))
+    pipeline_steps.append(
+        (
+            "classifier",
+            RandomForestClassifier(n_estimators=n_estimators, random_state=random_state),
         )
     )
     return Pipeline(steps=pipeline_steps)
--- src/9_evaluation_selection_project/train.py
+++ src/9_evaluation_selection_project/train.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import cross_val_score
 
 
 from .data import get_dataset
-from .pipeline import create_pipeline
+from .pipeline import create_pipeline_LogisticRegression
+from .pipeline import create_pipeline_RandomForest
 
 
 @click.command()
@@ -34,6 +35,10 @@ from .pipeline import create_pipeline
 )
 @click.option("--max-iter", default=100, type=int)
 @click.option("--logreg-c", default=1.0, type=float)
+@click.option("--use-feature-selection", default=0, type=int)
+@click.option("--ml-model", default=1, type=int)
+@click.option("--n-estimators", default=100, type=int)
+
 def train(
     dataset_path: Path,
     save_model_path: Path,
@@ -42,34 +47,42 @@ def train(
     use_scaler: bool,
     max_iter: int,
     logreg_c: float,
+    use_feature_selection: int,
+    ml_model: int,
+    n_estimators: int,
 ) -> None:
     features, target = get_dataset(dataset_path)
 
     with mlflow.start_run():
-        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+        if (ml_model == 1):
+            pipeline = create_pipeline_LogisticRegression(use_scaler, max_iter, logreg_c, use_feature_selection, random_state)
+            mlflow.log_param("max_iter", max_iter)
+            mlflow.log_param("logreg_c", logreg_c)
+            mlflow.log_param("ml_model", "LogisticRegression")
+        if (ml_model == 2):
+            pipeline = create_pipeline_RandomForest(use_scaler, n_estimators, use_feature_selection, random_state)
+            mlflow.log_param("n_estimators", n_estimators)
+            mlflow.log_param("ml_model", "RandomForestClassifier")
+
         pipeline.fit(features, target)
 
         cv = KFold(n_splits=test_split_ratio, shuffle=True, random_state=random_state)
 
         f1 = cross_val_score(pipeline, features, target, scoring='f1_weighted', cv=cv, n_jobs = 1).mean()
-        precision = cross_val_score(pipeline, features, target, scoring='precision_weighted', cv=cv, n_jobs = 1).mean()
-        recall = cross_val_score(pipeline, features, target, scoring='recall_weighted', cv=cv, n_jobs = 1).mean()
-        accuracy = cross_val_score(pipeline, features, target, scoring='accuracy', cv=cv, n_jobs = 1).mean()
+        
+        if use_feature_selection == 0:
+             mlflow.log_param("use_feature_selection", "None")
+        if use_feature_selection == 1:
+             mlflow.log_param("use_feature_selection", "SelectFromModel")
+        if use_feature_selection == 2:
+             mlflow.log_param("use_feature_selection", "VarianceThreshold")
 
         mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
-        mlflow.log_param("splits", test_split_ratio)
+        mlflow.log_param("splits", test_split_ratio)       
 
         mlflow.log_metric("f1-score", f1)
-        mlflow.log_metric("precision_score", precision)
-        mlflow.log_metric("recall_score", recall)
-        mlflow.log_metric("accuracy-score", accuracy)	
 
         click.echo(f"f1-score: {f1}.")
-        click.echo(f"precision_score: {precision}.")
-        click.echo(f"recall_score: {recall}.")
-        click.echo(f"accuracy-score: {accuracy}.")
 
         dump(pipeline, save_model_path)
         click.echo(f"Model is saved to {save_model_path}.")
