7132fae73377ad5a0a5b21306fa87fe9df44b80d
Таисия Титова <melodi63@yandex.ru>
2022-05-03 20:27:34 +0200

    change 9_evaluation_selection_project to evaluation_selection_project because of tests (error first number). Add Tests.

---
+++ src/evaluation_selection_project/data.py
@@ -0,0 +1,16 @@
+import logging
+from pathlib import Path
+from typing import Tuple
+
+import click
+import pandas as pd
+
+
+def get_dataset(
+    csv_path: Path
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    dataset = pd.read_csv(csv_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
+    features = dataset.drop("Cover_Type", axis=1)
+    target = dataset["Cover_Type"]
+    return features, target
---
+++ tests/test_evaluation_selection_project.py
@@ -0,0 +1,36 @@
+from click.testing import CliRunner
+import pytest
+from evaluation_selection_project.train import train
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+def test_error_for_invalid_test_split_ratio(
+    runner: CliRunner
+) -> None:
+    """It fails when test split ratio is less than 2."""
+    result = runner.invoke(
+        train,
+        [
+            "--test-split-ratio",
+            -1,
+        ],
+    )
+    assert result.exit_code == 1
+
+def test_success_for_correct_test_split_ratio(
+    runner: CliRunner
+) -> None:
+    """It success when test split ratio is more than 2."""
+    result = runner.invoke(
+        train,
+        [
+            "--max-iter",
+            1000,
+            "--test-split-ratio",
+            3,
+        ],     
+    )
+    assert result.exit_code == 0
